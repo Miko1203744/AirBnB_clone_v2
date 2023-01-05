dff1179ed06de61713535d450fbab266bd461445
Miko1203744 <106764762+Miko1203744@users.noreply.github.com>
2023-01-05 05:02:34 -0600

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,6 +11,12 @@ class FileStorage:
     def all(self):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
+     def delete(self, obj=None):
+        """Removes an object from the storage dictionary"""
+        if obj is not None:
+            obj_key = obj.to_dict()['__class__'] + '.' + obj.id
+            if obj_key in self.__objects.keys():
+                del self.__objects[obj_key]
 
     def new(self, obj):
         """Adds new object to storage dictionary"""

e126d599ff71d9695ac626610ff91d86a63621ea
Miko1203744 <106764762+Miko1203744@users.noreply.github.com>
2023-02-04 12:47:04 -0600

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,23 +4,23 @@ app=Flask(__name__)
 
 @app.route("/",strict_slashes=False)
 def Hello_hbnb():
-  return "Hello HBNB!"
+    return "Hello HBNB!"
 
 @app.route("/hbnb",strict_slashes=False)
 def hbnb():
-  return "HBNB"
+    return "HBNB"
 
 @app.route("/c/<text>",strict_slashes=False)
 def c(text):
-  text=text.replace("_"," ")
-  return "C {}".format(text)
+    text=text.replace("_"," ")
+    return "C {}".format(text)
 
 @app.route("/python/<text>",strict_slashes=False)
 def python(text="is cool"):
-  text=text.replace("_"," ")
-  return "python {}".format(text)
+    text=text.replace("_"," ")
+    return "python {}".format(text)
 if __name__="__main__":
-  app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
   
 
   

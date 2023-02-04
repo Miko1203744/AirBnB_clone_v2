edbac1af7782953c3192e4ef778d899c3c1d15ae
Miko1203744 <106764762+Miko1203744@users.noreply.github.com>
2023-02-04 06:13:58 -0600

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,6 +4,7 @@ from flask import Flask
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
+    /hbnb: Displays 'HBNB'.
 """
 app = Flask(__name__)
 @app.route("/", strict_slashes=False)
